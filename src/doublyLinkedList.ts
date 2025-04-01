180d1a92dfe6133cbab261ba9e32a3122e36eb3a
nevdaha2103 <vlabimir200628@gmail.com>
2025-04-01 21:20:56 +0300

    Add all the necessary functions

--- src/doublyLinkedList.ts
+++ src/doublyLinkedList.ts
@@ -1,54 +1,171 @@
-class Node {
+class DoublyNode {
   value: string;
-  next: Node | null = null;
-  prev: Node | null = null;
+  next: DoublyNode | null;
+  prev: DoublyNode | null;
 
   constructor(value: string) {
-    this.value = value;
+      this.value = value;
+      this.next = null;
+      this.prev = null;
   }
 }
 
-export class DoublyLinkedList {
-  private head: Node | null = null;
-  private tail: Node | null = null;
-  private size: number = 0;
+class DoublyLinkedList {
+  private head: DoublyNode | null;
+  private tail: DoublyNode | null;
+  private size: number;
+
+  constructor() {
+      this.head = null;
+      this.tail = null;
+      this.size = 0;
+  }
 
   length(): number {
-    return this.size;
+      return this.size;
   }
 
   append(element: string): void {
-    const newNode = new Node(element);
-    if (!this.head) {
-      this.head = this.tail = newNode;
-    } else {
-      this.tail!.next = newNode;
-      newNode.prev = this.tail;
-      this.tail = newNode;
-    }
-    this.size++;
+      const newNode = new DoublyNode(element);
+      if (!this.tail) {
+          this.head = this.tail = newNode;
+      } else {
+          this.tail.next = newNode;
+          newNode.prev = this.tail;
+          this.tail = newNode;
+      }
+      this.size++;
+  }
+
+  insert(element: string, index: number): void {
+      if (index < 0 || index > this.size) throw new Error("Index out of bounds");
+      const newNode = new DoublyNode(element);
+      if (index === 0) {
+          if (this.head) {
+              newNode.next = this.head;
+              this.head.prev = newNode;
+          }
+          this.head = newNode;
+          if (!this.tail) this.tail = newNode;
+      } else {
+          let current = this.head;
+          for (let i = 0; i < index - 1; i++) {
+              current = current!.next;
+          }
+          newNode.next = current!.next;
+          newNode.prev = current;
+          if (current!.next) {
+              current!.next.prev = newNode;
+          }
+          current!.next = newNode;
+          if (!newNode.next) this.tail = newNode;
+      }
+      this.size++;
   }
 
   delete(index: number): string {
-    if (index < 0 || index >= this.size) throw new Error("Invalid index");
-  
-    let current = this.head;
-    for (let i = 0; i < index; i++) {
-      current = current!.next;
-    }
-  
-    const value = current!.value;
-    if (current!.prev) current!.prev.next = current!.next;
-    if (current!.next) current!.next.prev = current!.prev;
-    if (index === 0) this.head = current!.next;
-    if (index === this.size - 1) this.tail = current!.prev;
-  
-    this.size--;
-    return value;
-  }  
+      if (index < 0 || index >= this.size) throw new Error("Index out of bounds");
+      let deletedValue: string;
+      if (index === 0) {
+          deletedValue = this.head!.value;
+          this.head = this.head!.next;
+          if (this.head) this.head.prev = null;
+          if (this.size === 1) this.tail = null;
+      } else {
+          let current = this.head;
+          for (let i = 0; i < index; i++) {
+              current = current!.next;
+          }
+          deletedValue = current!.value;
+          if (current!.prev) current!.prev.next = current!.next;
+          if (current!.next) current!.next.prev = current!.prev;
+          if (!current!.next) this.tail = current!.prev;
+      }
+      this.size--;
+      return deletedValue;
+  }
+
+  deleteAll(element: string): void {
+      let current = this.head;
+      while (current) {
+          if (current.value === element) {
+              if (current.prev) current.prev.next = current.next;
+              if (current.next) current.next.prev = current.prev;
+              if (current === this.head) this.head = current.next;
+              if (current === this.tail) this.tail = current.prev;
+              this.size--;
+          }
+          current = current.next;
+      }
+  }
+
+  get(index: number): string {
+      if (index < 0 || index >= this.size) throw new Error("Index out of bounds");
+      let current = this.head;
+      for (let i = 0; i < index; i++) {
+          current = current!.next;
+      }
+      return current!.value;
+  }
+
+  clone(): DoublyLinkedList {
+      const newList = new DoublyLinkedList();
+      let current = this.head;
+      while (current) {
+          newList.append(current.value);
+          current = current.next;
+      }
+      return newList;
+  }
+
+  reverse(): void {
+      let current = this.head;
+      let temp = null;
+      while (current) {
+          temp = current.prev;
+          current.prev = current.next;
+          current.next = temp;
+          current = current.prev;
+      }
+      
+      if (temp) {
+          this.tail = this.head;
+          this.head = temp.prev;
+      }
+  }
+
+  findFirst(element: string): number {
+      let current = this.head;
+      let index = 0;
+      while (current) {
+          if (current.value === element) return index;
+          current = current.next;
+          index++;
+      }
+      return -1;
+  }
+
+  findLast(element: string): number {
+      let current = this.tail;
+      let index = this.size - 1;
+      while (current) {
+          if (current.value === element) return index;
+          current = current.prev;
+          index--;
+      }
+      return -1;
+  }
 
   clear(): void {
-    this.head = this.tail = null;
-    this.size = 0;
+      this.head = this.tail = null;
+      this.size = 0;
+  }
+
+  extend(otherList: DoublyLinkedList): void {
+      let current = otherList.head;
+      while (current) {
+          this.append(current.value);
+          current = current.next;
+      }
   }
 }
