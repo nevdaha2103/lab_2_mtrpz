671cbc3a87a54eb8943fc47ca63e59709761890f
nevdaha2103 <vlabimir200628@gmail.com>
2025-04-01 14:16:36 +0300

    Add unit tests for DoublyLinkedList

---
+++ tests/doublyLinkedList.test.ts
@@ -0,0 +1,32 @@
+import { DoublyLinkedList } from "../src/doublyLinkedList";
+
+describe("DoublyLinkedList", () => {
+  let list: DoublyLinkedList;
+
+  beforeEach(() => {
+    list = new DoublyLinkedList();
+  });
+
+  test("should append elements and get correct length", () => {
+    list.append("A");
+    list.append("B");
+    expect(list.length()).toBe(2);
+  });
+
+  test("should delete element by index", () => {
+    list.append("A");
+    list.append("B");
+    expect(list.delete(1)).toBe("B");
+    expect(list.length()).toBe(1);
+  });
+
+  test("should clear the list", () => {
+    list.append("A");
+    list.clear();
+    expect(list.length()).toBe(0);
+  });
+
+  test("should throw error on invalid delete index", () => {
+    expect(() => list.delete(0)).toThrow("Invalid index");
+  });
+});
